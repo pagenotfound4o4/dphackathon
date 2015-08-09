527347ac5dd60179c27288283055aa25343b00b6
Unlucky <unlucky1990@gmail.com>
2015-08-09 10:25:58 +0800

    add dianping theme

---
+++ client/Dianping/app/src/main/java/org/emnets/dianping/ui/MainHomeFragment.java
@@ -0,0 +1,18 @@
+package org.emnets.dianping.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.emnets.dianping.R;
+
+public class MainHomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_home_fragment, container, false);
+        return view;
+    }
+}
---
+++ client/Dianping/app/src/main/java/org/emnets/dianping/ui/UserFragment.java
@@ -0,0 +1,15 @@
+package org.emnets.dianping.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
