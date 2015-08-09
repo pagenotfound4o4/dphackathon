47c4b3b28f1260fd814967265b2a63d48c618b5b
GuoKai <guok@emnets.org>
2015-08-09 12:48:58 +0800

    get favorite

---
+++ takeme/src/main/model/User.java
@@ -0,0 +1,55 @@
+package main.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7714827683995505928L;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "lover")
+	private String lover;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLover() {
+		return lover;
+	}
+
+	public void setLover(String lover) {
+		this.lover = lover;
+	}
+
+}
